8834c2e73e1c25c3142cb8742faaed8191272090
EtherSystem <dermeisternew@gmail.com>
2026-04-02 17:41:23 +0200

    Add a support for custom buffs so they dont generate Depression anymore (fix version)

--- TheLongMood/Core.cs
+++ TheLongMood/Core.cs
@@ -11,7 +11,7 @@ using static TheLongMood.Afflictions.Depression.Miserable;
 using static TheLongMood.Afflictions.Depression.Sad;
 using static TheLongMood.Afflictions.Depression.Weepy;
 
-[assembly: MelonInfo(typeof(TheLongMood.Core), "TheLongMood", "1.3.2", "EtherSystem, Flower Field", null)]
+[assembly: MelonInfo(typeof(TheLongMood.Core), "TheLongMood", "1.3.3", "EtherSystem, Flower Field", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace TheLongMood
